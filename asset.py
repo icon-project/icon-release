0b2cd4444c9263c913ad9762c0a3e9ba67458805
Seungmin Ryu <yakkle@gmail.com>
2021-09-30 16:24:32 +0900

    snap release with appstream metainfo (#12)
    - update artifact name with release tag

--- asset.py
+++ asset.py
@@ -1,8 +1,9 @@
-import pytz
 from datetime import datetime
+from string import Template
 
 
 def docker_tag() -> str:
+    import pytz
     tzinfo = pytz.timezone("Asia/Seoul")
     # hash_tag = "${{ hashFiles('Makefile') }}"
     # print(hash_tag)
@@ -17,3 +18,35 @@ def package_info(file_path: str) -> str:
 
     # https://git.io/JkD3W
     return f"PACKAGE_INFO_BODY<<EOF\n{body}\nEOF"
+
+
+def metadata(tag: str) -> str:
+    with open("./appstream.template", "r") as f:
+        template = f.read()
+
+    import re
+    import os
+    from pathlib import Path
+    from datetime import datetime
+
+    release_date = re.sub(r"\.\d+$", "", tag)
+    release_date = datetime.strptime(release_date, "%Y%m%d")
+    appstream_data = {
+        "NAME": "loopchain",
+        "SUMMARY": "loopchain citizen",
+        "DATE": f"{release_date:%Y-%m-%d}",
+        "VERSION": tag
+    }
+    t = Template(template)
+    appstream = t.substitute(appstream_data)
+
+    appstream_path: Path = Path("./snap/local/org.iconrepublic.loopchain.appdata.xml")
+    try:
+        appstream_path.parent.mkdir()
+    except FileExistsError:
+        pass
+
+    with open(appstream_path, "w") as f:
+        f.write(appstream)
+
+    return os.fspath(appstream_path)
